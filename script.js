f7c103c4e0810aa57373f9042aca661b46de3d73
Chin-Hsuan Wang <chwa4245@colorado.edu>
2023-09-21 12:23:28 -0600

    code upload, need to be checked before submit

---
+++ script.js
@@ -0,0 +1,16 @@
+// const usernameInput = document.getElementById('username');
+// usernameInput.addEventListener('input', () => {
+//   console.log(usernameInput.value);
+// });
+
+// const form = document.getElementById("form");
+// form.addEventListener("submit", function (event) {
+//     event.preventDefault(); // Prevents the default form submission
+//     // Your form validation and submission code here
+// });
+
+const button = document.getElementById("submit");
+button.addEventListener("click", function () {
+    // Your function code here
+    alert("Form Submitted!");
+});
